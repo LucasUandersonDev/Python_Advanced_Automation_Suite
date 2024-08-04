9c717fe83a2f4e949068f98e039e1ef8dea5c71f
LucasUandersonDev <lucasuanderson159@live.com>
2024-08-04 13:10:08 -0300

    Automação básica com pyautogui

--- Self_Managing_System/automated_task.py
+++ Self_Managing_System/automated_task.py
@@ -1,11 +1,18 @@
+'''
+PyAutoGUI é um módulo de automação de GUI multiplataforma
+Você pode controlar o mouse e o teclado, reconhecimento básico 
+de imagem para automatizar tarefas em seu computador.
+'''
 import pyautogui as positionMouse
 
+# Pausa de 2 segundos 
+positionMouse.sleep(2) 
+# Posição do mouse 
+print(positionMouse.position()) 
+# Move o cursor para a posição
+positionMouse.moveTo(x=801, y=1047) 
+# Clica na posição definida
+positionMouse.click(x=801, y=1047) 
 positionMouse.sleep(2)
-print(positionMouse.position())
-
-positionMouse.moveTo(x=801, y=1047)
-positionMouse.click(x=801, y=1047)
-
-positionMouse.sleep(2)
-
-positionMouse.typewrite("bloco")
+ # Digita o texto
+positionMouse.typewrite("bloco") 
